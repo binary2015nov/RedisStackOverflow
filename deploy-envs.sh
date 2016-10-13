2bc81e3642c6679faac31e60d3018f3f52a9298f
Darren Reid <darren.j.reid@gmail.com>
2016-10-13 18:25:07 +1100

    Fix domain

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -7,5 +7,5 @@ export IMAGE_VERSION=latest
 export AWS_DEFAULT_REGION=us-east-1
 export AWS_ECS_CLUSTER_NAME=default
 
-export AWS_VIRTUAL_HOST=redisstackoverflow.netcore.io.netcore.io
+export AWS_VIRTUAL_HOST=redisstackoverflow.netcore.io
 export AWS_REDIS_HOST=redis-redisstackoverflow
